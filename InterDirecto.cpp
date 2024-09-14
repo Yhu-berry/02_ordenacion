09295e2866f3e34c66ce775fe45e96b0fecbff67
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-09-14 17:34:50 -0500

    Agregando la funcion de interDirDer pero con elementos predeterminados

--- InterDirecto.cpp
+++ InterDirecto.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //Funcion que recorre el arreglo de Derecha a Izquierda
-int interDirDer(int ,int);
+void interDirDer(int[] ,int);
 
 //Funcion que recorre el arreglo de Izquierda a Derecha
 
@@ -11,21 +11,37 @@ int interDirDer(int ,int);
 //Funcion de intercambio bidireccional que recorre el arreglo usando las 3 fnciones anteriores 
 
 int main(){
-    int num;
-    cin>>num;
+    int num;   
+    cout<<"Ingrese el numero de elementos ( son 4, lista predeterminada): ";
+    cin>>num;  
 
     //definir un arreglo de 4 elementos 
     int Arr[]={25,10,20,6};
 
-    //imprimir arreglo original
+    //imprimir arreglo original (predeterminado)
     cout<<"Arreglo original: ";
     for(int i=0;i<num;i++){
         cout<<Arr[i]<<"\t";
     }
-    
+    cout<<"\n";
+    interDirDer(Arr,num);
+    cout<<"Arreglo ordenado: ";
+    for(int i=0;i<num;i++){
+        cout<<Arr[i]<<"\t";
+    }
+
     return 0;
 }
 
-int interDirDer(int A[],int n){
-
+void interDirDer(int A[],int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(A[j]>A[j+1]){
+                int aux;
+                aux=A[j];
+                A[j]=A[j+1];
+                A[j+1]=aux;
+            }
+        }
+    }
 }
