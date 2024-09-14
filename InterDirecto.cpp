505f726c8abea2b8155aa7350e247d2469ab21e7
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-09-13 21:58:39 -0500

    Imprimir lista desordenada en main

---
+++ InterDirecto.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+//Funcion que recorre el arreglo de Derecha a Izquierda
+int interDirDer(int ,int);
+
+//Funcion que recorre el arreglo de Izquierda a Derecha
+
+//Funcion de intercambio por señal 
+
+//Funcion de intercambio bidireccional que recorre el arreglo usando las 3 fnciones anteriores 
+
+int main(){
+    int num;
+    cin>>num;
+
+    //definir un arreglo de 4 elementos 
+    int Arr[]={25,10,20,6};
+
+    //imprimir arreglo original
+    cout<<"Arreglo original: ";
+    for(int i=0;i<num;i++){
+        cout<<Arr[i]<<"\t";
+    }
+    
+    return 0;
+}
+
+int interDirDer(int A[],int n){
+
+}
