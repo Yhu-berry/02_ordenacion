fdfbcf5cd35e6b1ca885926f80dffb5871a4bfaf
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-09-20 15:39:00 -0500

    Agregando la funcion de insercion usando bucles for en vez de while

--- insercion.cpp
+++ insercion.cpp
@@ -10,9 +10,26 @@ int main(){
     cout << "Ingrese los elementos: ";
     for(int i=0; i<num; i++)
         cin >> Arr[i];
-
+    
+    //Arreglo ordenado usando la fucion insercion
+    insercion(Arr, num);
+    cout << "Arreglo ordenado con metodo de insercion directa: ";
+    for(int i=0; i<num; i++)
+        cout << Arr[i] << " ";
+    cout << endl;
+    return 0;
 }
 
 int insercion(int A[100],int n){
-
+        int i, j, aux;
+    for(i=1; i<n; i++){
+        aux = A[i];
+        j = i-1;
+        while(j>=0 && A[j]>aux){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = aux;
+    }
+    return 0;
 }
