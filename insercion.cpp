cdf3ccc16b65142c5bf965439b8ecb5ff48cdaf6
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-09-20 15:34:04 -0500

    Solicitando los elementos del arreglo, lista desordenada

---
+++ insercion.cpp
@@ -0,0 +1,18 @@
+//Metodod de insercin directa
+#include <iostream>
+using namespace std;
+
+int insercion(int A[100],int n);
+int main(){
+    int num,Arr[100],aux;
+    cout << "Ingrese el numero de elementos: ";
+    cin >> num;
+    cout << "Ingrese los elementos: ";
+    for(int i=0; i<num; i++)
+        cin >> Arr[i];
+
+}
+
+int insercion(int A[100],int n){
+
+}
